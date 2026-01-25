89e44a9897b28fe32477665d45338dd3ecd3e093
syedowaisi <osyed8452@gmail.com>
2026-01-25 19:40:38 +0530

    var and datatype done

---
+++ 01basics/01variables.js
@@ -0,0 +1,18 @@
+const accountId = 14463
+let accountEmail = "owais@gmail.com"
+var accountPassword = "91829"
+accountCity = "Unnao"
+let accountState;
+
+accountEmail ="syed@gmail.com"
+accountPassword = "38728"
+accountCity = "kanpur"
+
+console.log(accountId)
+
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01basics/02datatype.js
@@ -0,0 +1,23 @@
+"use strict" //treat all js code as newer version
+
+//alert(3+3) not run since we are using nodejs, not browser
+
+console.log(3+3)
+
+let name="owais"
+let age = 18
+let isLoggedIn = true
+
+//number=> 2 to power 53 it's range
+//bigint
+// string=>""
+//boolean
+//null=> standalone value
+//undefined ,when value not assign
+//symbol=> unique
+
+//object
+
+console.log(typeof "owais");
+console.log(typeof null);
+
