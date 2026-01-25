9a34caef2f9cf6bc7f6d958a5ffcb3e74890f3a7
syedowaisi <osyed8452@gmail.com>
2026-01-26 00:46:14 +0530

    memory advantage

--- 01basics/05data.js
+++ 01basics/05data.js
@@ -12,3 +12,28 @@ const myfunc=function(){
     console.log("Hello World");
 }
 
+// ******************memory assign**************************
+
+// stack (used in primitive)  ,heap(used in non primitive)
+//in stack we get a copy but in heap we get a reference
+
+let myYoutubeName="syedowaisdotcom"
+
+let anothername=myYoutubeName
+anothername="chaiaurcode"
+
+console.log(myYoutubeName);
+console.log(anothername);
+
+
+let userOne={
+    email:"syed@.com",
+    upi:"osyedokicici",
+}
+
+let userTwo=userOne
+
+userTwo.email="owais@.com"
+console.log(userOne.email);
+console.log(userTwo.email);//both have same email due to same reference in heap
+
