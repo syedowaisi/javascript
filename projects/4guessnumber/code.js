62ef5f63b07004fbe16e0e52dfa938244a983045
syedowaisi <osyed8452@gmail.com>
2026-01-30 21:59:24 +0530

    project done

---
+++ projects/4guessnumber/code.js
@@ -0,0 +1,7 @@
+const randomnum=console.log(parseInt(Math.random()*100 +1 ));
+
+const submit=document.querySelector('#subt')
+const userinput=document.querySelector('#guessField')
+const guesses=document.querySelector('#guessField')
+
+
